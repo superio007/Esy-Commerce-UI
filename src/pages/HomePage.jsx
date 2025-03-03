7bea3cfb6caad548e997609efcc6505aebad9c47
Kiran GIG <dhokekiran88@gmail.com>
2025-03-03 17:13:44 +0530

    Home page done

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,31 @@
 import "../css/Homepage.css";
+import HeroSection from "../components/HeroSection";
+import BulletPoints from "../components/BulletPoints";
+import CustomerSlider from "../components/CustomerSlider";
+import Certification from "../components/CertificationSlider";
 import BlogGrid from "../components/BlogGrid";
+import Testimonial from "../components/Testimonial";
+import Headless from "../components/HeadlessCms";
+import CMS from "../components/Cms";
 const HomePage = () => {
   return (
     <>
+      {/* Hero Section */}
+      <HeroSection />
+      {/* Brand Slider */}
+      <CustomerSlider />
+      {/* Bullet Points */}
+      <BulletPoints />
+      {/* CMS Section */}
+      <CMS />
+      {/* Headless Cms section */}
+      <Headless />
+      {/* Blog Grid */}
       <BlogGrid />
+      {/* Testimonial */}
+      <Testimonial />
+      {/* Awards & Certification */}
+      <Certification />
     </>
   );
 };
