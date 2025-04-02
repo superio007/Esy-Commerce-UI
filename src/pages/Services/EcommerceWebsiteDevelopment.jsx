9f0c74fa7ca0f76b54fe4719bbee183a9c7da4c3
Kiran Dhoke <dhokekiran98@gmail.com>
2025-04-02 19:04:54 +0530

    feat: implement API integration for EcommerceEnablement and EcommerceWebsiteDevelopment components to enhance dynamic content rendering

--- src/pages/Services/EcommerceWebsiteDevelopment.jsx
+++ src/pages/Services/EcommerceWebsiteDevelopment.jsx
@@ -7,17 +7,45 @@ import WhyUs from "../../components/services/whyChooseUS";
 import BrandSlider from "../../components/HomePage/CustomerSlider";
 import CaseStudiesSection from "../../components/CaseStudies";
 import FAQ from "../../components/services/FAQ";
+import axios from "axios";
+import { useQuery } from "@tanstack/react-query";
+import EcommerceWebsiteDevelopmentPageData from "../Data/EcommerceWebsiteDevelopmentData.json";
+const fetchEcommerceWebsiteDevelopmentContent = async () => {
+  const { data } = await axios.get(
+    "http://localhost:1337/api/ecommerce-website-development?populate[service_hero_section][populate]=*&populate[service_over_view][populate]=*&populate[key_benift][populate]=*&populate[how_it_work][populate]=*&populate[why_us][populate]=*&populate[faq][populate]=*&populate[cta][populate]=*"
+  );
+  return data.data;
+};
 const EcommerceWebsiteDevelopment = () => {
+  const { data, isLoading, error } = useQuery({
+    queryKey: ["EcommerceWebsiteDevelopmentpage-content"],
+    queryFn: fetchEcommerceWebsiteDevelopmentContent,
+  });
+
+  // Use API data if available; fallback to static data on error
+  const apiResponse = error
+    ? EcommerceWebsiteDevelopmentPageData.data || []
+    : data || [];
+  if (isLoading) return <p>Loading...</p>;
   return (
     <>
-      <HeroSection />
-      <OverviewKeyBenifits />
-      <Steps />
-      <WhyUs />
-      <BrandSlider />
-      <CaseStudiesSection bgColor={"#ffffff"} />
-      <FAQ />
-      <CTA />
+      {apiResponse ? (
+        <>
+          <HeroSection apiRes={apiResponse.service_hero_section} />
+          <OverviewKeyBenifits
+            overview={apiResponse.service_over_view}
+            keyBenifits={apiResponse.key_benift}
+          />
+          <Steps apiRes={apiResponse.how_it_work} />
+          <WhyUs apiRes={apiResponse.why_us} />
+          <BrandSlider />
+          <CaseStudiesSection bgColor={"#ffffff"} />
+          <FAQ apiRes={apiResponse.faq} />
+          <CTA apiRes={apiResponse.cta} />
+        </>
+      ) : (
+        <div>No data available</div>
+      )}
     </>
   );
 };
